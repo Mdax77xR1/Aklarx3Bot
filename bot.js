afe1d023701baaa2016131d441ce3f8bd2ba4059
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2018-12-01 10:59:10 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-​const Discord = require('discord.js');  
+const Discord = require('discord.js');
 const db = require('quick.db');  
 const hastebin = require('hastebin-gen');  
 const client = new Discord.Client();    
@@ -19,8 +19,8 @@ const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const sql = require("sqlite");
- const dateFormat = require('dateformat'); 
- const pretty = require('pretty-ms') 
+const dateFormat = require('dateformat'); 
+const pretty = require('pretty-ms') 
 ,ti={}  
 ,spee={}; 
 
